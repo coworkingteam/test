63f0f80f48eb1bae5766cb33f05c0c455edd422a
neznayka <neznaykagm@gmail.com>
2023-02-25 17:36:58 +0200

    fix footer

--- modules/shared/components/ui/footer/index.tsx
+++ modules/shared/components/ui/footer/index.tsx
@@ -104,13 +104,6 @@ const Footer = () => {
         <FAQWrapper>© aksis 2023</FAQWrapper>
 
         <FAQWrapper>
-          to firma, której branża została w Polskiej Klasyfikacji Działalności (PKD) sklasyfikowana jako: Działalność
-          związana z wyszukiwaniem miejsc pracy i pozyskiwaniem pracowników. Powstała w 2021 roku. Forma prawna firmy
-          BAGHEERA (R&K GROUP) SP. Z O.O. to Spółka z ograniczoną odpowiedzialnością. Firma posiada numer NIP
-          7822895535, numer REGON 388431682 i KRS 0000889522, a jej siedziba mieści się pod adresem: Ul. Stanisława
-          Małachowskiego 8/PI, 61-129 w Poznaniu, województwo Wielkopolskie. Z firmą BAGHEERA (R&K GROUP) SP. Z O.O.
-          skontaktujesz się telefonicznie pod numerem +48 728 000 702. Odwiedź stronę internetową firmy BAGHEERA (R&K
-          GROUP) SP. Z O.O., lub wyślij wiadomość na adres e-mail{' '}
           <a href='mailto:support@aksis.agency'>support@aksis.agency</a>
         </FAQWrapper>
       </ContentWrapper>
